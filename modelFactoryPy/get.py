3914db57c7ac1c96baa532029889fd90b271ee64
jmkeynes <vechtomova.maria@gmail.com>
2016-11-17 19:49:27 +0100

    bug: n and n_na in getSummary as int

--- modelFactoryPy/get.py
+++ modelFactoryPy/get.py
@@ -12,6 +12,8 @@ def getSummary(df):
     for i in a.index:
         n_na.append(len(df[i])-df[i].count())
     a["n_na"] = n_na
+    a['n'] = a['n'].apply(lambda x: int(x))
+    a['n_na'] = a['n_na'].apply(lambda x: int(x))
     a["variable"] = a.index
     return a.reset_index(drop = True)
 
